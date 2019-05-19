3c8a30647b3dd23c88e3b8eaa40d619b84e8e1e0
LmeEspinosa <lucasespinosa.ungs@gmail.com>
2019-05-18 23:30:43 -0300

    Add project files.

---
+++ HangFireTest/Global.asax.cs
@@ -0,0 +1,41 @@
+﻿using Hangfire;
+using Hangfire.SqlServer;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace HangFireTest
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        private IEnumerable<IDisposable> GetHangfireServers()
+        {
+            GlobalConfiguration.Configuration
+                .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+                .UseSimpleAssemblyNameTypeSerializer()
+                .UseRecommendedSerializerSettings()
+                .UseSqlServerStorage(System.Configuration.ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString, new SqlServerStorageOptions
+                {
+                    CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+                    SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+                    QueuePollInterval = TimeSpan.Zero,
+                    UseRecommendedIsolationLevel = true,
+                    UsePageLocksOnDequeue = true,
+                    PrepareSchemaIfNecessary = true,
+                    DisableGlobalLocks = true
+                });
+
+            yield return new BackgroundJobServer();
+        }
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            HangfireAspNet.Use(GetHangfireServers);
+        }
+    }
+}
---
+++ HangFireTest/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Hangfire;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(HangFireTest.Startup))]
+
+namespace HangFireTest
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseHangfireDashboard("/tareas");
+            //var jobId = BackgroundJob.Enqueue(
+            //    () => Console.WriteLine("Fire-and-forget!"));
+            RecurringJob.AddOrUpdate(
+                () => Console.WriteLine("Recurring!"),
+                Cron.Daily);
+        }
+    }
+}
